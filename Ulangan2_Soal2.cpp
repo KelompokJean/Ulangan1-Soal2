8603fb8f7eee0da95e416a717fd98318e379aa79
KelompokJean <inforjjmv@gmail.com>
2024-08-07 11:59:02 +0700

    Add files via upload

---
+++ Ulangan2_Soal2.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Kelompok Jean";
+	cin.ignore();
+	cout << endl;
+	cout << "NIS" << endl;
+	cout << "1. 00000012\n"
+		"2. 00000014\n"
+		"3. 00000020\n"
+		"4. 00000027" << endl;
+	cin.ignore();
+	cout << "Nama" << endl;
+	cout << "1. Jean Aurelia\n"
+		"2. Joan Edsel Rivano\n"
+		"3. Marc Sebastian\n"
+		"4. Vellensya" << endl;
+	cin.ignore();
+	cout << "Kelas 11.2" << endl;
+	cin.ignore();
+	cout << "Jurusan IPA" << endl;
+	cin.ignore();
+	cout << "Jenis Kelamin" << endl;
+	cout << "1. Jean Aurelia (P)\n"
+		"2. Joan Edsel Rivano (L)\n"
+		"3. Marc Sebastian (L)\n"
+		"4. Vellensya (P)" << endl;
+	cin.ignore();
+	cout << "Alamat" << endl;
+	cout << "1. Jean Aurelia - Perumnas Blok B\n"
+		"2. Joan Edsel Rivano - Citra Permata Regency Cikampek\n"
+		"3. Marc Sebastian - Grand Taruma Purnawarman 07\n"
+		"4. Vellensya - Cluster Mediterania Galuh Mas" << endl;
+	system("PAUSE");
+	return 0;
+}
